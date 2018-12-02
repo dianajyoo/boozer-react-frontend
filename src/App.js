1dcb61444334d978fb63a90e80014b45ffa83a62
dianajyoo <diana.yooj@gmail.com>
2018-12-02 18:19:34 -0500

    added change handler to form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CocktailsContainer from './CocktailsContainer'
 import Form from './Form'
 import logo from './logo.svg';
--- src/Form.js
+++ src/Form.js
@@ -8,7 +8,9 @@ class Form extends React.Component {
     instructions: ''
   }
 
-  submitHandler = (e) => {
+  handleSubmit = (e) => {
+    e.preventDefault()
+
     fetch('http://localhost:3000/api/v1/cocktails', {
       method: 'POST',
       headers: {
@@ -16,32 +18,39 @@ class Form extends React.Component {
       },
       body: JSON.stringify(this.state)
     })
-      .then(res => res.json())
-      .then(json => this.setState({
-        [e.target.name]: e.target.value
-      }))
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.value)
+
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
     return (
       <div className='form'>
         <h2>Create a Cocktail</h2>
-        <form onSubmit={(e) => this.submitHandler(e)}>
+        <form onSubmit={this.handleSubmit}>
           <input
             type='text'
             name='name'
             placeholder='Name'
-            value={this.state.name} /><br />
+            value={this.state.name}
+            onChange={this.handleChange} /><br />
           <input
             type='text'
             name='description'
             placeholder='Description'
-            value={this.state.description} /><br />
+            value={this.state.description}
+            onChange={this.handleChange} /><br />
           <input
             type='text'
             name='instructions'
             placeholder='Instruction'
-            value={this.state.instructions} /><br />
+            value={this.state.instructions}
+            onChange={this.handleChange} /><br />
 
           <input type='submit' value='Create Cocktail' />
         </form>
