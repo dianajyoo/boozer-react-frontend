1f7ac823387ed4677927dd6b4b6667d0ba7bc8d7
dianajyoo <diana.yooj@gmail.com>
2018-12-02 16:11:38 -0500

    submit handler added

--- src/Form.js
+++ src/Form.js
@@ -1,18 +1,53 @@
 import React from 'react'
 
-const Form = (props) => {
-  return (
-    <div className='form'>
-      <h2>Create a Cocktail</h2>
-      <form>
-        <input type='text' name='name' placeholder='Name' /><br />
-        <input type='text' name='description' placeholder='Description' /><br />
-        <input type='text' name='instructions' placeholder='Instruction' /><br />
+class Form extends React.Component {
 
-        <input type='submit' />
-      </form>
-    </div>
-  )
+  state = {
+    name: '',
+    description: '',
+    instructions: ''
+  }
+
+  submitHandler = (e) => {
+    fetch('http://localhost:3000/api/v1/cocktails', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+      .then(res => res.json())
+      .then(json => this.setState({
+        [e.target.name]: e.target.value
+      }))
+  }
+
+  render() {
+    return (
+      <div className='form'>
+        <h2>Create a Cocktail</h2>
+        <form onSubmit={(e) => this.submitHandler(e)}>
+          <input
+            type='text'
+            name='name'
+            placeholder='Name'
+            value={this.state.name} /><br />
+          <input
+            type='text'
+            name='description'
+            placeholder='Description'
+            value={this.state.description} /><br />
+          <input
+            type='text'
+            name='instructions'
+            placeholder='Instruction'
+            value={this.state.instructions} /><br />
+
+          <input type='submit' value='Create Cocktail' />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Form
