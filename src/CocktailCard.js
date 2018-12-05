6451dabbf8944db6a44496c0c3ad7eef20002c59
dianajyoo <diana.yooj@gmail.com>
2018-12-05 14:11:58 -0500

    updated to include instructions

--- src/CocktailCard.js
+++ src/CocktailCard.js
@@ -8,7 +8,9 @@ const CocktailCard = ({match, cocktail, cocktailDetail}) => {
     cocktail.id === parseInt(match.params.cocktailId) ? (<div className='card'>
       <h1>{cocktail.name}</h1>
       <h3>{cocktail.description}</h3>
-      <h3>Ingredients</h3>
+      <h2>Instructions</h2>
+      <h3>{cocktail.instructions}</h3>
+      <h2>Ingredients</h2>
       <ul>{detailArray}</ul>
     </div>) : null
   )
